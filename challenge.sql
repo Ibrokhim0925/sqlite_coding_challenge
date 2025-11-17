55d0eae225123ac27ddfbc8b513d86143713798e
Ibrokhim Ushurov <ibrokhimushurov@usf.edu>
2025-11-16 23:12:16 -0500

    Complete Task 2 - Revenue by Category (incl. variant)

--- challenge.sql
+++ challenge.sql
@@ -13,3 +13,33 @@ GROUP BY
 ORDER BY
     total_spend DESC
 LIMIT 5;
+
+-- TASK 2: Total Revenue by Product Category
+SELECT
+    p.category,
+    SUM(oi.quantity * oi.unit_price) AS revenue
+FROM
+    products AS p
+JOIN
+    order_items AS oi ON p.id = oi.product_id
+GROUP BY
+    p.category
+ORDER BY
+    revenue DESC;
+
+-- TASK 2 (Variant): Revenue for 'Delivered' Orders Only
+SELECT
+    p.category,
+    SUM(oi.quantity * oi.unit_price) AS revenue
+FROM
+    products AS p
+JOIN
+    order_items AS oi ON p.id = oi.product_id
+JOIN
+    orders AS o ON oi.order_id = o.id
+WHERE
+    o.status = 'Delivered'
+GROUP BY
+    p.category
+ORDER BY
+    revenue DESC;

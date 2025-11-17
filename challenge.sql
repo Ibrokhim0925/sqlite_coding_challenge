910a0ce12e6e7d22f2cf4933040e19d73d8a4523
Ibrokhim Ushurov <ibrokhimushurov@usf.edu>
2025-11-16 23:09:53 -0500

    Complete Task 1 - Top 5 Customers by Spend

--- challenge.sql
+++ challenge.sql
@@ -1,7 +1,15 @@
--- Task 1 
-SELECT *
-FROM customers;
-
--- Task 2
-Select *
-From departments;
+-- TASK 1: Top 5 Customers by Total Spend
+SELECT
+    c.first_name || ' ' || c.last_name AS customer_full_name,
+    SUM(oi.quantity * oi.unit_price) AS total_spend
+FROM
+    customers AS c
+JOIN
+    orders AS o ON c.id = o.customer_id
+JOIN
+    order_items AS oi ON o.id = oi.order_id
+GROUP BY
+    c.id -- Grouping by customer ID is safer than by name
+ORDER BY
+    total_spend DESC
+LIMIT 5;
